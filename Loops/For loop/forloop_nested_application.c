1c51b103d90ed16b815f7801a4c718d9f3e5de6a
Simon <41785978+SimonAndro@users.noreply.github.com>
2020-05-26 01:30:46 +0800

    Update forloop_nested_application.c

--- Loops/For loop/forloop_nested_application.c
+++ Loops/For loop/forloop_nested_application.c
@@ -5,17 +5,15 @@ int main()
 {
     int i;
 
-    for (i = 1;i<11; i ++)
+   printf("Multiplication Table\n");
+    for(i = 1; i<=10; i++ )
     {
-
-        for(j = 1; j<11; j++)
+        for(j = 1; j<=10; j++)
         {
-            printf("%2d x %2d = %2d\n",i,j,i*j);
+            printf("%d x %2d = %2d\n",i,j,i*j);
         }
         printf("\n");
-
     }
-
 
     return 0;
 }
