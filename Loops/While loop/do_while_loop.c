fa6cfa244b901a738719ee2abb98858ce57e7c7c
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2020-06-02 01:29:24 +0800

    While loop and the do while loop
    watch video at youtube, url:https://youtu.be/DmzJ8iQ-OpI

---
+++ Loops/While loop/do_while_loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int i;
+    i = 10;
+
+    do
+    {
+        printf("the value of i  is %d\n",i);
+        //i = i -1;
+        i--;
+
+    }while(i>8);
+    return 0;
+}
+
---
+++ Loops/While loop/while_loop.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    x = 10;
+    while(x>0)
+    {
+        printf("the value of x is %d\n", x);
+        x--;
+    }
+    return 0;
+}
